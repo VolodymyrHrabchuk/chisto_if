770d485de076201ee4c832b4ca0377236b226c52
“VolodymyrHrabchuk” <vladimirgrbchk2804@gmail.com>
2024-02-12 13:45:28 +0200

    fixing some issues

--- client/src/components/Cleaners.jsx
+++ client/src/components/Cleaners.jsx
@@ -1,22 +1,36 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import CleanersData from "./CleanersData";
 
 const Cleaners = () => {
+  const swiperRef = useRef(null);
   const swiperCleanPrev = useRef(null);
   const swiperCleanNext = useRef(null);
 
+  useEffect(() => {
+    if (
+      swiperRef.current &&
+      swiperCleanPrev.current &&
+      swiperCleanNext.current
+    ) {
+      swiperRef.current.params.navigation.prevEl = swiperCleanPrev.current;
+      swiperRef.current.params.navigation.nextEl = swiperCleanNext.current;
+      swiperRef.current.navigation.init();
+      swiperRef.current.navigation.update();
+    }
+  }, [swiperRef, swiperCleanPrev, swiperCleanNext]);
+
   return (
-    <div className="container">
-      <h2 className="heading">Очисники, які ми використовуємо</h2>
-      <h4 className="subheading">
+    <div className='container'>
+      <h2 className='heading'>Очисники, які ми використовуємо</h2>
+      <h4 className='subheading'>
         Для ефективного видалення плям, для кожного типу необхідно
         використовувати спеціалізовані очисники
       </h4>
       <motion.div
-        className="cleaner-sw"
+        className='cleaner-sw'
         initial={{ opacity: 0 }}
         whileInView={{
           opacity: 1,
@@ -56,7 +70,7 @@ const Cleaners = () => {
               spaceBetween: 30,
             },
           }}
-          className="clean-swiper"
+          className='clean-swiper'
           onInit={(swiper) => {
             swiper.params.navigation.prevEl = swiperCleanPrev.current;
             swiper.params.navigation.nextEl = swiperCleanNext.current;
@@ -66,18 +80,18 @@ const Cleaners = () => {
         >
           {CleanersData.map((CleanersData, index) => (
             <SwiperSlide key={index}>
-              <div className="card-item__item df">
-                <img src={CleanersData.img} alt="" className="card-item__img" />
-                <div className="card-item__descr">
-                  <h4 className="card-item__type">{CleanersData.type}</h4>
-                  <p className="card-item__brand">{CleanersData.brand}</p>
+              <div className='card-item__item df'>
+                <img src={CleanersData.img} alt='' className='card-item__img' />
+                <div className='card-item__descr'>
+                  <h4 className='card-item__type'>{CleanersData.type}</h4>
+                  <p className='card-item__brand'>{CleanersData.brand}</p>
                 </div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="swiperCleanPrev" ref={swiperCleanPrev}></div>
-        <div className="swiperCleanNext" ref={swiperCleanNext}></div>
+        <div className='swiperCleanPrev' ref={swiperCleanPrev}></div>
+        <div className='swiperCleanNext' ref={swiperCleanNext}></div>
       </motion.div>
     </div>
   );
